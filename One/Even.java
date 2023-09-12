6de75c18d3efcb44540507a127a05c0e3a0855b7
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-12 23:13:53 +0530

    Update Even.java

--- One/Even.java
+++ One/Even.java
@@ -17,3 +17,24 @@ public class Even {
     }
 }
 
+Algorithm
+
+Step 1: Import the Scanner class and declare a class named "Even."
+
+Step 2: Define the main method within the "Even" class. This method serves as the entry point for the program.
+
+Step 3: Declare an integer variable a to store the user input.
+
+Step 4: Create a new Scanner object named sc to read input from the keyboard.
+
+Step 5: Display a prompt to the user, asking them to enter a number to check if it's even or odd.
+
+Step 6: Read an integer input from the user using the nextInt method of the Scanner object and store it in the variable a.
+
+Step 7: Check if the entered number a is even or odd by using the modulo operator %. If a % 2 equals 0, it means the number is even.
+
+Step 8: If the condition in Step 7 is true, print a message indicating that the number is even, including the number itself.
+
+Step 9: If the condition in Step 7 is false (i.e., the number is not even), print a message indicating that the number is odd, including the number itself.
+
+Step 10: End
