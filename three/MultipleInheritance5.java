15e98d1902798e45e59345e7d6df73edbf5e70eb
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-12 23:51:36 +0530

    Update MultipleInheritance5.java

--- three/MultipleInheritance5.java
+++ three/MultipleInheritance5.java
@@ -45,3 +45,28 @@ class MultipleInheritance{
 		obj.display();
 	}	
 }
+
+
+Algorithm
+
+Step 1: Start
+Step 2: Create an interface Sports with a method 'Activities' that takes a float parameter
+'s'
+Step 3: Create an interface Student with a method 'Marks' that takes three parameters
+'m' (marks), 'r' (register number), and 'n' (name)
+Step 4: Create a class Result that implements both Sports and Student interfaces
+Step 5: Add instance variables 'marks','registerNo','score', and 'Name' to the Result class
+Step 6: Implement the 'Activities' method in the Result class to set the 'score' variable
+Step 7: Implement the 'Marks' method in the Result class to set the 'marks', 'registerNo',
+and 'Name' variables
+Step 8: Create a method 'display' in the Result class to display the student's name,
+register number, marks, and score
+Step 9: Create the main method in the MultipleInheritance class
+Step 10: Read the student details from the user (name, register number, marks, and
+score)
+Step 11: Create an object 'obj' of Result class
+39
+Step 12: Call the 'Activities' method on 'obj' and pass the score
+Step 13: Call the 'Marks' method on 'obj' and pass the marks, register number, and name
+Step 14: Call the 'display' method on 'obj' to display the student's details
+Step 15: Stop
