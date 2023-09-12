1c944818d71ee760e7252b107a0ed51a73d3ea7f
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-12 23:42:28 +0530

    Update BillingSystem.java

--- three/BillingSystem.java
+++ three/BillingSystem.java
@@ -59,3 +59,44 @@ class BillingSystem{
         System.out.println("\t\t\t\t\t\t\tNet Amount="+netAmount);
     }
 }
+
+
+Algorithm
+
+Step 1: Define an interface named Calculations with a single method calculate().
+
+Step 2: Define a Java class named Product that implements the Calculations interface:
+
+a. Declare instance variables for productId, name, quantity, unitPrice, and total.
+
+b. Create a constructor for the Product class:
+- Prompt the user to enter product details, including productId, name, quantity, and unitPrice.
+- Calculate the total cost of the product using the calculate() method and store it in the total variable.
+
+c. Define a method display() to display product details in a formatted manner.
+
+d. Implement the calculate() method to calculate the total cost (unitPrice * quantity) and return the result.
+
+Step 3: Define a Java class named BillingSystem:
+
+a. Declare a static double variable netAmount to keep track of the total cost of all products.
+
+b. In the main method:
+- Prompt the user to enter an orderNo.
+- Get the current date and time using the Date class.
+
+c. Prompt the user to enter the number of products (pCount) they want to purchase.
+
+d. Create an array of Product objects named product with a size of pCount.
+
+e. Use a for loop to create Product objects and store them in the product array.
+
+f. Display an order summary, including order number, date, and a table header.
+
+g. Iterate through the product array and call the display() method for each product to display product details in a table format.
+
+h. Calculate the netAmount by summing up the total values of all products.
+
+i. Display the netAmount as the "Net Amount."
+
+Step 4: End
