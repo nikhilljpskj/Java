0b922a9faa927af1d7c9da283fe7945a6982d06f
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-12 23:39:05 +0530

    Update Abstract.java

--- three/Abstract.java
+++ three/Abstract.java
@@ -61,3 +61,53 @@ class Abstract {
         sc.close();
     }
 }
+
+Algorithm
+
+    Step 1: Define an abstract Java class named Shape.
+
+Step 2: Inside the Shape class, declare an abstract method named Findarea(), which will be used to calculate and display the area of various shapes.
+
+Step 3: Define a Java class named Rectangle that extends the Shape class:
+
+a. Declare instance variables length and breadth to represent the dimensions of the rectangle.
+
+b. Create a constructor for the Rectangle class that takes the length and breadth as parameters and initializes the instance variables.
+
+c. Override the Findarea() method to calculate and display the area of the rectangle.
+
+Step 4: Define a Java class named Square that extends the Shape class:
+
+a. Declare an instance variable side to represent the side length of the square.
+
+b. Create a constructor for the Square class that takes the side length as a parameter and initializes the instance variable.
+
+c. Override the Findarea() method to calculate and display the area of the square.
+
+Step 5: Define a Java class named Circle that extends the Shape class:
+
+a. Declare an instance variable radius to represent the radius of the circle.
+
+b. Create a constructor for the Circle class that takes the radius as a parameter and initializes the instance variable.
+
+c. Override the Findarea() method to calculate and display the area of the circle.
+
+Step 6: In the Abstract class (the main class):
+
+a. Create a Scanner object sc to read input from the user.
+
+b. Prompt the user to enter the radius of the circle and read the value into the radius variable.
+
+c. Create an object obj of the Circle class, passing the radius as a parameter, and call the Findarea() method to calculate and display the area of the circle.
+
+d. Prompt the user to enter the side length of the square and read the value into the side variable.
+
+e. Create an object obj1 of the Square class, passing the side as a parameter, and call the Findarea() method to calculate and display the area of the square.
+
+f. Prompt the user to enter the length and breadth of the rectangle and read the values into the length and breadth variables.
+
+g. Create an object obj2 of the Rectangle class, passing the length and breadth as parameters, and call the Findarea() method to calculate and display the area of the rectangle.
+
+h. Close the Scanner object sc.
+
+Step 7: End
