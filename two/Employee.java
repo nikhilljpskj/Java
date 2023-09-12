088397be2385f0a4ae33d29015d1fa7679d95f99
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-12 23:26:21 +0530

    Update Employee.java

--- two/Employee.java
+++ two/Employee.java
@@ -52,3 +52,39 @@ class Employee
 		}
 	}
 }
+
+Algorithm
+
+Step 1: Define a Java class named Employee.
+
+Step 2: Inside the Employee class, declare the main method, which is the entry point of the program.
+
+Step 3: Create a Scanner object sc to read input from the user.
+
+Step 4: Prompt the user to enter the number of employees to be entered using System.out.print and read the value into the variable length.
+
+Step 5: Create an array of EmployeData objects named array with a size of length.
+
+Step 6: Use a for loop to initialize each element of the array by creating a new EmployeData object and storing it in the corresponding array index.
+
+Step 7: Prompt the user to enter a search key (Employee number) using System.out.print and read the value into the variable searchKey.
+
+Step 8: Call the searchArray method, passing the array and searchKey as arguments to search for the employee with the given employee number.
+
+Step 9: Define a static method named searchArray that takes an array of EmployeData objects (array) and an integer sk (search key) as parameters.
+
+Step 10: Inside the searchArray method, declare integer variables i and flag to be used as loop counters and a flag to indicate whether the employee was found or not.
+
+Step 11: Use a for loop to iterate through the array of EmployeData objects.
+
+Step 12: Inside the loop, check if the Eno (Employee number) of the current EmployeData object matches the sk (search key). If it does, set the flag to 1 and break out of the loop.
+
+Step 13: After the loop, check the value of the flag. If flag is 1, print the details of the employee with the specified employee number, including their name and salary.
+
+Step 14: If flag is not 1, print a message indicating that the employee was not found.
+
+Step 15: Define a static nested class named EmployeData within the Employee class to represent employee data. This class has integer fields for Employee number (Eno), salary (Salary), and a string field for Employee name (Ename).
+
+Step 16: Inside the EmployeData class, create a constructor to initialize the Eno, Ename, and Salary fields by reading input from the user using the Scanner object sc.
+
+Step 17: End
